8a2843aec405d0445ec2087534c0aacbcfdd0428
starsyork <zhouchenjie@outlook.com>
2017-02-02 14:52:50 -0800

    add item table

--- languages/zh-cn.py
+++ languages/zh-cn.py
@@ -1,4 +1,4 @@
-# coding: utf8
+# -*- coding: utf-8 -*-
 {
 '!langcode!': 'zh-cn',
 '!langname!': '中文',
@@ -9,6 +9,7 @@
 '%Y-%m-%d': '%Y-%m-%d',
 '%Y-%m-%d %H:%M:%S': '%Y-%m-%d %H:%M:%S',
 '(something like "it-it")': '(格式类似 "zh-tw")',
+'@markmin\x01An error occured, please [[reload %s]] the page': 'An error occured, please [[reload %s]] the page',
 'A new version of web2py is available': '新版 web2py 已推出',
 'A new version of web2py is available: %s': '新版 web2py 已推出: %s',
 'about': '关于',
@@ -237,6 +238,7 @@
 'Welcome to web2py': '欢迎使用 web2py',
 'Welcome to web2py!': '欢迎使用 web2py!',
 'Which called the function %s located in the file %s': 'Which called the function %s located in the file %s',
+'Working...': 'Working...',
 'YES': '是',
 'You are successfully running web2py': '您已成功运行 web2py',
 'You can modify this application and adapt it to your needs': '请根据您的需要修改本程序',
